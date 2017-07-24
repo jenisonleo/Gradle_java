565e60025121f547a1b62f122d20055af6c5cfb7
jenison-3631 <jenison.gracious@zohocorp.com>
2017-07-24 23:37:30 +0530

    java files

---
+++ src/main/java/jenison/Jenison.java
@@ -0,0 +1,10 @@
+package jenison;
+
+
+public class Jenison {
+    public static void main(String[] params){
+        System.out.println("params "+params[0]);
+        Leonard leonard=new Leonard();
+        leonard.printJenison();
+    }
+}
---
+++ src/main/java/jenison/Leonard.java
@@ -0,0 +1,11 @@
+package jenison;
+
+public class Leonard{
+
+    public Leonard(){
+        
+    }
+    public void printJenison(){
+        System.out.println("jenison");
+    }
+}
