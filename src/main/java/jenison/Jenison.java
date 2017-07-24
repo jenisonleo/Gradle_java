7a2fe0fd36636106aafe52bb31ad0ba5e5599b3a
jenisonleo <jenisongracious@gmail.com>
2017-07-24 23:48:27 +0530

    Update Jenison.java

--- src/main/java/jenison/Jenison.java
+++ src/main/java/jenison/Jenison.java
@@ -3,7 +3,6 @@ package jenison;
 
 public class Jenison {
     public static void main(String[] params){
-        System.out.println("params "+params[0]);
         Leonard leonard=new Leonard();
         leonard.printJenison();
     }
